b6696031a0de3dcec1b4d4bc8481637cab695b3f
ch1hyun <och2510@gmail.com>
2023-09-28 11:20:12 +0900

    WEEK 1

---
+++ week-1/Baemin-Clone/Baemin-Clone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Baemin-Clone
+//
+//  Created by Oh Chi Hyun on 2023/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("helloword")
+    }
+
+
+}
+
