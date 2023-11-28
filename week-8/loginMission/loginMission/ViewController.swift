9e92fc91515d8287e339e9aeb4e26a605911e546
ch1hyun <och2510@gmail.com>
2023-11-29 01:27:45 +0900

    UserDefaults를 이용한 로그인 구현 수정

--- week-8/loginMission/loginMission/ViewController.swift
+++ week-8/loginMission/loginMission/ViewController.swift
@@ -25,40 +25,46 @@ class ViewController: UIViewController {
     
     @IBAction func joinButtonDidTap(_ sender: UIButton) {
         if (checkInputs() == false) {
-            return;
+            return
+        }
+        let id = idTextField.text
+        let pw = pwTextField.text
+        
+        if let object = UserDefaults.standard.object(forKey: (id)!) {
+            resultLabel.text = "이미 \((id)!)의 아이디가 존재합니다."
+            return
         }
         
-        if let object = UserDefaults.standard.object(forKey: idTextField.text ?? "") {
-            resultLabel.text = "이미 \(idTextField.text ?? "")의 아이디가 존재합니다."
-            return;
-        }
+        UserDefaults.standard.set((pw)!, forKey: (id)!)
         
-        UserDefaults.standard.set(pwTextField.text ?? "", forKey: idTextField.text ?? "")
-        
-        resultLabel.text = "회원가입 성공\n아이디 : \(idTextField.text ?? "")\n비밀번호 : \(pwTextField.text ?? "")"
+        resultLabel.text = "회원가입 성공\n아이디 : \((id)!)\n비밀번호 : \((pw)!)"
         idTextField.text = ""
         pwTextField.text = ""
     }
     
     @IBAction func loginButtonDidTap(_ sender: UIButton) {
         if (checkInputs() == false) {
-            return;
+            return
         }
+        let id = idTextField.text
+        let pw = pwTextField.text
         
-        if let pw = UserDefaults.standard.string(forKey: idTextField.text ?? "") {
+        if let object = UserDefaults.standard.object(forKey: (id)!) {
             if pw == pwTextField.text {
-                resultLabel.text = "로그인에 성공하였습니다.\n로그인 아이디 : \(idTextField.text ?? "")"
+                resultLabel.text = "로그인에 성공하였습니다. \n로그인 아이디 : \((id)!)"
                 idTextField.text = ""
                 pwTextField.text = ""
-                return;
+                return
             }
             
             resultLabel.text = "비밀번호가 틀렸습니다."
-            return;
+            return
         }
         
         resultLabel.text = "아이디가 존재하지 않습니다."
+        UserDefaults.standard.
     }
+    
     
     
     private func checkInputs() -> Bool {
